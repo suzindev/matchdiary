bb56996b01f318912214c2bcdebfd38ddf5f1ffd
Fernando Suzin <suzin.dev@gmail.com>
2024-02-27 21:22:25 -0300

    feat: add initial backend

---
+++ server/src/main/java/br/com/suzintech/matchdiary/MatchdiaryApplication.java
@@ -0,0 +1,13 @@
+package br.com.suzintech.matchdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchdiaryApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/br/com/suzintech/matchdiary/MatchdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.suzintech.matchdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
